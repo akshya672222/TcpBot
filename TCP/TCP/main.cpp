16f6d6e99d39600c4c822de43f8e5a8d225bfbe2
Akshay Sunderwani <akshya672222@gmail.com>
2017-10-13 18:30:38 -0400

    Initial commit

---
+++ TCP/TCP/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  TCP
+//
+//  Created by AKSHAY SUNDERWANI on 13/10/17.
+//  Copyright © 2017 AKSHAY SUNDERWANI. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
