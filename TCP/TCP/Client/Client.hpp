164202d16156d63b617d8c7b30b074b740cf9f36
Sudhansh Aggarwal <sudhansh@truckx.com>
2017-10-13 21:15:36 -0400

    Initial commit

---
+++ TCP/TCP/Client/Client.hpp
@@ -0,0 +1,14 @@
+//
+//  Client.hpp
+//  TCP
+//
+//  Created by Sudhansh Aggarwal on 10/13/17.
+//  Copyright © 2017 AKSHAY SUNDERWANI. All rights reserved.
+//
+
+#ifndef Client_hpp
+#define Client_hpp
+
+#include <stdio.h>
+
+#endif /* Client_hpp */
---
+++ client/client.c
@@ -0,0 +1,7 @@
+//TCP Client for chat
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<>
